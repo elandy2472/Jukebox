393030a9536ba104695d571d07e9bc08bf17df35
Camilo Rivera <102061094+JCamiloRiveraS@users.noreply.github.com>
2024-09-13 16:35:24 -0500

    Correccion RegisterController PDO
    Se modificó las variables directas y se implemento PDO para la conexión a la base de datos

--- app/controllers/registerController.php
+++ app/controllers/registerController.php
@@ -1,46 +1,64 @@
 <?php
 
 $servername = "localhost";
-$username = "root"; 
-$password = ""; 
+$username = "root";
+$password = "";
 $dbname = "jukebox";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error); // Verificar la conexión
-}
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {   // Recoge datos del form de register-view
+try {
  
-    $name1 = $_POST['name1'];
-    $name2 = $_POST['name2'];
-    $last1 = $_POST['last1'];
-    $last2 = $_POST['last2'];
-    $email = $_POST['email'];
-    $cc = $_POST['cc'];
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $nameEnter = $_POST['nameEnter'];
-    $address = $_POST['address'];
-    $city = $_POST['city'];
-    $nit = $_POST['nit'];
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 
-    // Insertar en la tabla empresa
-    $sql_empresa = "INSERT INTO empresa (nit, nombre, direccion, ciudad) VALUES ('$nit', '$nameEnter', '$address', '$city')";
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // Insertar en la tabla usuarioempresa
-    $sql_usuarioempresa = "INSERT INTO usuarioempresa (documento, nombres, apellidos, correo, usuario, contrasena, nit) VALUES ('$cc', '$name1 $name2', '$last1 $last2', '$email', '$username', '$password', '$nit')";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {  
+        
+        $name1 = $_POST['name1'];
+        $name2 = $_POST['name2'];
+        $last1 = $_POST['last1'];
+        $last2 = $_POST['last2'];
+        $email = $_POST['email'];
+        $cc = $_POST['cc'];
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $nameEnter = $_POST['nameEnter'];
+        $address = $_POST['address'];
+        $city = $_POST['city'];
+        $nit = $_POST['nit'];
 
-    if ($conn->query($sql_empresa) === TRUE && $conn->query($sql_usuarioempresa) === TRUE) {
+   
+        $sql_empresa = "INSERT INTO empresa (nit, nombre, direccion, ciudad) VALUES (:nit, :nameEnter, :address, :city)";
+        $stmt_empresa = $conn->prepare($sql_empresa);
+        $stmt_empresa->bindParam(':nit', $nit);
+        $stmt_empresa->bindParam(':nameEnter', $nameEnter);
+        $stmt_empresa->bindParam(':address', $address);
+        $stmt_empresa->bindParam(':city', $city);
+        $stmt_empresa->execute();
+
+       
+        $sql_usuarioempresa = "INSERT INTO usuarioempresa (documento, nombres, apellidos, correo, usuario, contrasena, nit) 
+                               VALUES (:cc, :nombres, :apellidos, :email, :username, :password, :nit)";
+        $stmt_usuarioempresa = $conn->prepare($sql_usuarioempresa);
+        $stmt_usuarioempresa->bindParam(':cc', $cc);
+        $stmt_usuarioempresa->bindParam(':nombres', $name1_full);
+        $stmt_usuarioempresa->bindParam(':apellidos', $last1_full);
+        $stmt_usuarioempresa->bindParam(':email', $email);
+        $stmt_usuarioempresa->bindParam(':username', $username);
+        $stmt_usuarioempresa->bindParam(':password', $password);
+        $stmt_usuarioempresa->bindParam(':nit', $nit);
+
+        $name1_full = $name1 . ' ' . $name2;
+        $last1_full = $last1 . ' ' . $last2;
+
+        $stmt_usuarioempresa->execute();
+
         echo "Registro exitoso, Redireccionar a vista home";
-    } else {
-        echo "Error: " . $sql_empresa . "<br>" . $conn->error;
-        echo "Error: " . $sql_usuarioempresa . "<br>" . $conn->error;
     }
-
-    $conn->close();
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
 }
-?>
+
+
+$conn = null;
+
 ?>
